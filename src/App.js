afe1eb0fe4603d8b23fcd9cae181ba4b274bc368
Gopi Boddeda Venkata <gopib@idc.campsystems.com>
2022-07-15 17:20:30 +0530

    Initial version of the app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div style={{display: 'flex' , alignItems: 'center', justifyContent: 'center', color: 'red'}}>
+    <h2>Welcome to the Recct E-Commerce APP!!</h2>
+   </div>
   );
 }
 
